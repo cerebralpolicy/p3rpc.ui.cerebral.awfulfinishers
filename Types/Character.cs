7e33153786ff95c68285e71768d6ad51c6bc63af
cole.wintringham@gmail.com <cole.wintringham@gmail.com>
2024-12-28 17:11:10 -0800

    Randomizer

---
+++ Types/Character.cs
@@ -0,0 +1,39 @@
+﻿namespace p3rpc.ui.cerebral.awfulfinishers.Types
+{
+    public enum Character
+    {
+        None = 0,
+        Player,
+        Yukari,
+        Stupei,
+        Akihiko,
+        Mitsuru,
+        Fuuka,
+        Aigis,
+        Ken,
+        Koromaru,
+        Shinjiro,
+        Metis,
+        AigisDLC
+    }
+    public static class Characters
+    {
+        public static string GetName(Character character)
+        {
+            static string name_num(Character c)
+                => $"{c}";
+            if (character == Character.Stupei)
+            {
+                return $"Junpei";
+            }
+            else if (character == Character.AigisDLC)
+            {
+                return $"Aigis (EA)";
+            }
+            else
+            {
+                return name_num(character);
+            }
+        }
+    }
+}
